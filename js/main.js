bcfbb7a5fad40adc3703eff949f79bb91503f8cd
coffeedrunk <dan@danryan.tv>
2015-11-19 10:11:23 +1300

    removed login and sign

---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function() {
+    
+    smoothScroll.init({
+        speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInOutCubic', // Easing pattern to use
+    });
+    
+})();
